25fcad4a8dab5dbadcdb5255de4493cab6c9f9af
Laszlo Sragner <sragner@gmail.com>
2023-05-15 18:18:26 +0100

    .

--- Step06/babyagi.py
+++ Step06/babyagi.py
@@ -53,13 +53,12 @@ class BabyAGI:
     def add_task(self, task):
         self.task_list.append(task)
 
-
-    def task_creation_agent(objective, result, task_description, task_list):
+    def task_creation_agent(self, result, task_description):
         prompt = TASK_CREATION_PROMPT.format(
-            objective=objective,
+            objective=self.objective,
             result=result,
             task_description=task_description,
-            task_list=', '.join([t['task_name'] for t in task_list]),
+            task_list=', '.join([t['task_name'] for t in self.task_list]),
         )
         response = openai.Completion.create(
             engine='text-davinci-003',
@@ -71,17 +70,20 @@ class BabyAGI:
             presence_penalty=0,
         )
         new_tasks = response.choices[0].text.strip().split('\n')
-        return [{'task_name': task_name} for task_name in new_tasks]
+        return [
+            {'task_name': task_name}
+            for task_name in new_tasks  # <-- dictionary instead of class, 'primitive obsession'?
+        ]
 
-
-    def prioritization_agent(this_task_id):
-        global task_list
-        task_names = [t['task_name'] for t in task_list]
+    def prioritization_agent(self, this_task_id):
+        task_names = [t['task_name'] for t in self.task_list]
         next_task_id = int(this_task_id) + 1
-        prompt = PRIORITIZATION_PROMPT.format(task_names=task_names, objective=OBJECTIVE, next_task_id=next_task_id)
+        prompt = PRIORITIZATION_PROMPT.format(
+            task_names=task_names, objective=self.objective, next_task_id=next_task_id
+        )
         response = openai.Completion.create(
-            engine='text-davinci-003',
-            prompt=prompt,
+            engine='text-davinci-003',  #                 <-- this API call is similar to the previous function's
+            prompt=prompt,  #                             <-- maybe extract a common function with defaults
             temperature=0.5,
             max_tokens=1000,
             top_p=1,
@@ -89,20 +91,19 @@ class BabyAGI:
             presence_penalty=0,
         )
         new_tasks = response.choices[0].text.strip().split('\n')
-        task_list = deque()
-        for task_string in new_tasks:
+        self.task_list = deque()
+        for task_string in new_tasks:  #                  <-- loop with a lot of stuff, maybe comprehensions?
             task_parts = task_string.strip().split('.', 1)
             if len(task_parts) == 2:
                 task_id = task_parts[0].strip()
                 task_name = task_parts[1].strip()
-                task_list.append({'task_id': task_id, 'task_name': task_name})
+                self.task_list.append({'task_id': task_id, 'task_name': task_name})  # <-- dictionary again
 
-
-    def execution_agent(objective, task):
-        context = context_agent(index=YOUR_TABLE_NAME, query=objective, n=5)
+    def execution_agent(self, task) -> str:
+        context = self.context_agent(query=self.objective, n=5)
         response = openai.Completion.create(
             engine='text-davinci-003',
-            prompt=EXECUTION_PROMPT.format(objective=objective, task=task),
+            prompt=EXECUTION_PROMPT.format(objective=self.objective, task=task),
             temperature=0.7,
             max_tokens=2000,
             top_p=1,
@@ -111,36 +112,43 @@ class BabyAGI:
         )
         return response.choices[0].text.strip()
 
-
-    def context_agent(query, index, n):
-        query_embedding = get_ada_embedding(query)
-        index = pinecone.Index(index_name=index)
-        results = index.query(query_embedding, top_k=n, include_metadata=True)
+    def context_agent(self, query, n):
+        query_embedding = self.ai_service.get_ada_embedding(query)
+        results = self.vector_service.index.query(query_embedding, top_k=n, include_metadata=True)
         sorted_results = sorted(results.matches, key=lambda x: x.score, reverse=True)
         return [(str(item.metadata['task'])) for item in sorted_results]
 
+    def run(self, first_task):
+        print(self.objective)
+        first_task = {'task_id': 1, 'task_name': first_task}
+        self.add_task(first_task)
+        task_id_counter = 1
+        for _ in range(4):
+            if self.task_list:
+                task = self.task_list.popleft()
+                print(task['task_name'])
+                result = self.execution_agent(task['task_name'])
+                print(result)
+                this_task_id = int(task['task_id'])
+                enriched_result = {'data': result}
+                result_id = f'result_{task["task_id"]}'
+                vector = enriched_result['data']
+                self.vector_service.index.upsert(
+                    [
+                        (
+                            result_id,
+                            self.ai_service.get_ada_embedding(vector),
+                            {'task': task['task_name'], 'result': result},
+                        )
+                    ]
+                )
+            new_tasks = self.task_creation_agent(enriched_result, task['task_name'])
 
-    first_task = {'task_id': 1, 'task_name': YOUR_FIRST_TASK}
-
-    add_task(first_task)
-    task_id_counter = 1
-    for _ in range(4):
-        if task_list:
-            task = task_list.popleft()
-            result = execution_agent(OBJECTIVE, task['task_name'])
-            this_task_id = int(task['task_id'])
-            enriched_result = {'data': result}
-            result_id = f'result_{task["task_id"]}'
-            vector = enriched_result['data']
-            index.upsert([(result_id, get_ada_embedding(vector), {'task': task['task_name'], 'result': result})])
-
-        new_tasks = task_creation_agent(OBJECTIVE, enriched_result, task['task_name'], [t['task_name'] for t in task_list])
-
-        for new_task in new_tasks:
-            task_id_counter += 1
-            new_task.update({'task_id': task_id_counter})
-            add_task(new_task)
-        prioritization_agent(this_task_id)
+            for new_task in new_tasks:
+                task_id_counter += 1
+                new_task.update({'task_id': task_id_counter})
+                self.add_task(new_task)
+            self.prioritization_agent(this_task_id)
 
 
 def main():
